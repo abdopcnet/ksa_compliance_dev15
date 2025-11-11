70b1b2053b4a125c9360b0c49d1c2929e00133f2
abdopcnet <abdopcnet@gmail.com>
2025-11-11 23:30:53 +0300

    Standardize string quotes in sales_invoice.js for consistency and improved readability

--- ksa_compliance/public/js/sales_invoice.js
+++ ksa_compliance/public/js/sales_invoice.js
@@ -1,24 +1,19 @@
-frappe.ui.form.on("Sales Invoice", {
+frappe.ui.form.on('Sales Invoice', {
   setup: function (frm) {
-    frm.set_query(
-      "custom_return_against_additional_references",
-      function (doc) {
-        // Similar to logic in erpnext/public/js/controllers/transaction.js for return_against
-        let filters = {
-          docstatus: 1,
-          is_return: 0,
-          company: doc.company,
-        };
-        if (frm.fields_dict["customer"] && doc.customer)
-          filters["customer"] = doc.customer;
-        if (frm.fields_dict["supplier"] && doc.supplier)
-          filters["supplier"] = doc.supplier;
+    frm.set_query('custom_return_against_additional_references', function (doc) {
+      // Similar to logic in erpnext/public/js/controllers/transaction.js for return_against
+      let filters = {
+        docstatus: 1,
+        is_return: 0,
+        company: doc.company,
+      };
+      if (frm.fields_dict['customer'] && doc.customer) filters['customer'] = doc.customer;
+      if (frm.fields_dict['supplier'] && doc.supplier) filters['supplier'] = doc.supplier;
 
-        return {
-          filters: filters,
-        };
-      }
-    );
+      return {
+        filters: filters,
+      };
+    });
   },
   async refresh(frm) {
     await set_zatca_integration_status(frm);
@@ -34,7 +29,7 @@ async function set_zatca_discount_reason(frm) {
 async function set_zatca_integration_status(frm) {
   const res = await frappe.call({
     method:
-      "ksa_compliance.ksa_compliance.doctype.sales_invoice_additional_fields.sales_invoice_additional_fields.get_zatca_integration_status",
+      'ksa_compliance.ksa_compliance.doctype.sales_invoice_additional_fields.sales_invoice_additional_fields.get_zatca_integration_status',
     args: {
       invoice_id: frm.doc.name,
       doctype: frm.doc.doctype,
@@ -43,11 +38,11 @@ async function set_zatca_integration_status(frm) {
 
   const status = res.integration_status;
   if (status) {
-    let color = "blue";
-    if (status === "Accepted") {
-      color = "green";
-    } else if (["Rejected", "Resend"].includes(status)) {
-      color = "red";
+    let color = 'blue';
+    if (status === 'Accepted') {
+      color = 'green';
+    } else if (['Rejected', 'Resend'].includes(status)) {
+      color = 'red';
     }
     frm.set_intro(`<b>Zatca Status: ${status}</b>`, color);
   }
@@ -55,7 +50,7 @@ async function set_zatca_integration_status(frm) {
 
 async function get_zatca_discount_reason_codes() {
   const res = await frappe.call({
-    method: "ksa_compliance.invoice.get_zatca_invoice_discount_reason_list",
+    method: 'ksa_compliance.invoice.get_zatca_invoice_discount_reason_list',
   });
   return res.message;
 }
