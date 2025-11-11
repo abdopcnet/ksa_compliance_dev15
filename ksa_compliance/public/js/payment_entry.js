4fcebc65dbeef351d1046be68507ac58c9798fd7
abdopcnet <abdopcnet@gmail.com>
2025-11-11 23:30:48 +0300

    Standardize string quotes in payment_entry.js for consistency and improved readability

--- ksa_compliance/public/js/payment_entry.js
+++ ksa_compliance/public/js/payment_entry.js
@@ -1,4 +1,4 @@
-frappe.ui.form.on("Payment Entry", {
+frappe.ui.form.on('Payment Entry', {
   async refresh(frm) {
     if (frm.doc.custom_prepayment_invoice && frm.doc.docstatus == 1) {
       await set_zatca_integration_status(frm);
@@ -9,7 +9,7 @@ frappe.ui.form.on("Payment Entry", {
 async function set_zatca_integration_status(frm) {
   const res = await frappe.call({
     method:
-      "ksa_compliance.ksa_compliance.doctype.sales_invoice_additional_fields.sales_invoice_additional_fields.get_zatca_integration_status",
+      'ksa_compliance.ksa_compliance.doctype.sales_invoice_additional_fields.sales_invoice_additional_fields.get_zatca_integration_status',
     args: {
       invoice_id: frm.doc.name,
       doctype: frm.doc.doctype,
@@ -18,11 +18,11 @@ async function set_zatca_integration_status(frm) {
 
   const status = res.integration_status;
   if (status) {
-    let color = "blue";
-    if (status === "Accepted") {
-      color = "green";
-    } else if (["Rejected", "Resend"].includes(status)) {
-      color = "red";
+    let color = 'blue';
+    if (status === 'Accepted') {
+      color = 'green';
+    } else if (['Rejected', 'Resend'].includes(status)) {
+      color = 'red';
     }
     frm.set_intro(`<b>Zatca Status: ${status}</b>`, color);
   }
