bcb511f7080a2f3a5bc9c7a1d7ecc56da09efac8
abdopcnet <abdopcnet@gmail.com>
2025-11-12 16:25:02 +0300

    Add compare_sales_with_zatca report module

---
+++ ksa_compliance/ksa_compliance/report/compare_sales_with_zatca/compare_sales_with_zatca.js
@@ -0,0 +1,31 @@
+// Copyright (c) 2025, LavaLoon and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["compare_Sales_with_zatca"] = {
+    "filters": [
+        {
+            "fieldname": "from_date",
+            "label": "من تاريخ",
+            "fieldtype": "Date",
+            "default": frappe.datetime.month_start()
+        },
+        {
+            "fieldname": "to_date",
+            "label": "إلى تاريخ",
+            "fieldtype": "Date",
+            "default": frappe.datetime.now_date()
+        },
+        {
+            "fieldname": "customer",
+            "label": "العميل",
+            "fieldtype": "Link",
+            "options": "Customer"
+        },
+        {
+            "fieldname": "integration_status",
+            "label": "حالة التكامل",
+            "fieldtype": "Select",
+            "options": "\nAccepted\nAccepted with warnings\nNot Sended\nNo Sales Invoice"
+        }
+    ]
+};
