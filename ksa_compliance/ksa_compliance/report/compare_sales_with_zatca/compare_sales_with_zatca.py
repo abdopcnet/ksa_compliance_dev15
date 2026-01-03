6978fedb4fe6a8abd74db76624cc69602f4a2d47
abdopcnet <abdopcnet@gmail.com>
2026-01-03 23:04:00 +0000

    23-04-03-01-2026

--- ksa_compliance/ksa_compliance/report/compare_sales_with_zatca/compare_sales_with_zatca.py
+++ ksa_compliance/ksa_compliance/report/compare_sales_with_zatca/compare_sales_with_zatca.py
@@ -5,54 +5,54 @@ import frappe
 
 
 def execute(filters=None):
-	# تعريف الأعمدة
-	columns = [
-		{
-			"fieldname": "posting_date",
-			"label": "Date",
-			"fieldtype": "Date",
-			"width": 130
-		},
-		{
-			"fieldname": "name",
-			"label": "Invoice",
-			"fieldtype": "Link",
-			"options": "Sales Invoice",
-			"width": 170
-		},
-		{
-			"fieldname": "customer",
-			"label": "Customer",
-			"fieldtype": "Link",
-			"options": "Customer",
-			"width": 180
-		},
-		{
-			"fieldname": "grand_total",
-			"label": "Grand Total",
-			"fieldtype": "Data",
-			"width": 130
-		},
-		{
-			"fieldname": "total_taxes_and_charges",
-			"label": "Tax Amount",
-			"fieldtype": "Data",
-			"width": 130
-		},
-		{
-			"fieldname": "integration_status",
-			"label": "(ZATCA)",
-			"fieldtype": "Data",
-			"width": 200
-		}
-	]
+    # Define columns
+    columns = [
+        {
+            "fieldname": "posting_date",
+            "label": "Date",
+            "fieldtype": "Date",
+            "width": 130
+        },
+        {
+            "fieldname": "name",
+            "label": "Invoice",
+            "fieldtype": "Link",
+            "options": "Sales Invoice",
+            "width": 170
+        },
+        {
+            "fieldname": "customer",
+            "label": "Customer",
+            "fieldtype": "Link",
+            "options": "Customer",
+            "width": 180
+        },
+        {
+            "fieldname": "grand_total",
+            "label": "Grand Total",
+            "fieldtype": "Data",
+            "width": 130
+        },
+        {
+            "fieldname": "total_taxes_and_charges",
+            "label": "Tax Amount",
+            "fieldtype": "Data",
+            "width": 130
+        },
+        {
+            "fieldname": "integration_status",
+            "label": "(ZATCA)",
+            "fieldtype": "Data",
+            "width": 200
+        }
+    ]
 
-	from_date = filters.get("from_date") or frappe.datetime.month_start()
-	to_date = filters.get("to_date") or frappe.datetime.now_date()
-	customer = filters.get("customer")
-	integration_status = filters.get("integration_status")
+    from_date = filters.get("from_date") or frappe.datetime.month_start()
+    to_date = filters.get("to_date") or frappe.datetime.now_date()
+    customer = filters.get("customer")
+    integration_status = filters.get("integration_status")
 
-	query = """
+    query = """
 	SELECT
 	  `tabSales Invoice`.`posting_date`,
 	  `tabSales Invoice`.`name`,
@@ -71,20 +71,20 @@ def execute(filters=None):
 	  ON `tabSales Invoice Additional Fields`.`sales_invoice` = `tabSales Invoice`.`name`
 	"""
 
-	where_conditions = []
-	params = {}
+    where_conditions = []
+    params = {}
 
-	if from_date and to_date:
-		where_conditions.append("`tabSales Invoice`.`posting_date` BETWEEN %(from_date)s AND %(to_date)s")
-		params["from_date"] = from_date
-		params["to_date"] = to_date
+    if from_date and to_date:
+        where_conditions.append("`tabSales Invoice`.`posting_date` BETWEEN %(from_date)s AND %(to_date)s")
+        params["from_date"] = from_date
+        params["to_date"] = to_date
 
-	if customer:
-		where_conditions.append("`tabSales Invoice`.`customer` = %(customer)s")
-		params["customer"] = customer
+    if customer:
+        where_conditions.append("`tabSales Invoice`.`customer` = %(customer)s")
+        params["customer"] = customer
 
-	if integration_status:
-		where_conditions.append("""
+    if integration_status:
+        where_conditions.append("""
 		(
 		    CASE
 		        WHEN `tabSales Invoice Additional Fields`.`sales_invoice` = `tabSales Invoice`.`name`
@@ -95,11 +95,11 @@ def execute(filters=None):
 		    END
 		) = %(integration_status)s
 		""")
-		params["integration_status"] = integration_status
+        params["integration_status"] = integration_status
 
-	if where_conditions:
-		query = query + " WHERE " + " AND ".join(where_conditions)
+    if where_conditions:
+        query = query + " WHERE " + " AND ".join(where_conditions)
 
-	mydata = frappe.db.sql(query, params, as_dict=True)
+    mydata = frappe.db.sql(query, params, as_dict=True)
 
-	return columns, mydata
+    return columns, mydata
