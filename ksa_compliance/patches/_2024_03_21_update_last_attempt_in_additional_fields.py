daf084da26ab28fbf40f495d1c9bc6e1efb42107
abdopcnet <abdopcnet@gmail.com>
2026-01-10 12:00:00 +0000

    12-00-10-01-2026

--- ksa_compliance/patches/_2024_03_21_update_last_attempt_in_additional_fields.py
+++ ksa_compliance/patches/_2024_03_21_update_last_attempt_in_additional_fields.py
@@ -4,7 +4,7 @@ import frappe
 def execute():
     print('Updating last attempt with the last modified date.')
     frappe.db.sql("""
-        UPDATE `tabSales Invoice Additional Fields` 
-        SET last_attempt = modified 
+        UPDATE `tabSales Invoice Additional Fields`
+        SET last_attempt = modified
         WHERE last_attempt IS NULL
     """)
